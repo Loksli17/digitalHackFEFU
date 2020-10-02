b7a4d73c05b2881b7e11831f43e250be738b5d27
Alexander <sansanichick@gmail.com>
2020-10-02 16:23:55 +1000

    vue ajax

--- server/index.js
+++ server/index.js
@@ -10,12 +10,11 @@ const
     app  = express(),
     port = config.app.port || process.env.port;
 
-app.use('/api/index', require('./routers/index.js'));
-
 app.use(morgan('combined'));
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
-app.use(cors());
 
+app.use('/api/index', require('./routers/index.js'));
 
-app.listen(port, () => console.log(`server has been started in port ${3000}`));
+app.listen(port, () => console.log(`server has been started on port ${3000}`));
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,42 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <div class="home">
+        <img alt="Vue logo" src="../assets/logo.png" />
+        <h3>{{ stuff }}</h3>
+    </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+    // @ is an alias to /src
+    import axios from "axios";
 
-export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
-}
+    export default {
+        name: 'Home',
+        data() {
+            return {
+                stuff: ""
+            }
+        },
+        mounted() {
+            this.fetchData();
+        },
+        methods: {
+            async fetchData() {
+                const config = {
+                    headers: {
+                        "Access-Control-Allow-Origin": "*"
+                    }
+                }
+
+                try {
+                    const res = await axios.get("http://localhost:3000/api/index", config);
+                    this.stuff = await res.data;
+                } catch (error) {
+                    console.error(error);
+                }
+            }
+        }
+    }
 </script>
+
+<style lang="scss">
+</style>
