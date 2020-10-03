9d441b7ee61e5c2b3f5f471eaba81788ece86b07
Uxa <kirill.yu99@gmail.com>
2020-10-03 17:56:56 +1000

    praaaavilno

--- src/components/ProductsBin/product.vue
+++ src/components/ProductsBin/product.vue
@@ -15,12 +15,9 @@
                 <span>{{ product.sname }}</span>
             </div>
             <span id="price">{{ product.price }} ₽</span>
-            <div class="order-button">
-                В корзину
+            <div class="third-bin">
+                <button @click="AddToCart">В корзину</button>
             </div>
-        </div>
-        <div class="third-bin">
-            <button @click="AddToCart">Добавить в корзину</button>
         </div>
     </li>
 </template>
@@ -58,9 +55,12 @@
         padding: 20px;
         margin: 5px;
         box-shadow: 7px 7px 5px 0px rgba(50, 50, 50, 0.2);
+        font-family: 'Exo 2', sans-serif;
 
         .image {
             background: no-repeat center;
+            width: 90%;
+            height: 270px;
         }
 
         .product-info {
@@ -81,16 +81,28 @@
                 }
             }
 
+            .third-bin {
+                background-color: #4476CC;
+
+                button {
+                    background: none;
+                    border: 0px;
+                    color: white;
+                    font-size: 14px;
+                    text-align: center;
+                    line-height: 30px;
+                }
+
+                &:hover {
+                    background: red;
+                }
+            }
+
             #price {
                 line-height: 35px;
                 font-weight: bold;
             }
         }
-    }
-
-    .image {
-        width: 90%;
-        height: 270px;
     }
 
     .order-button {
